bb49b01e250a4d1d06fadd5e8b9446f7ce8a230c
Tom <tomalahamed0@gmail.com>
2022-02-13 23:06:27 +0600

    ghgg

--- carshop/settings.py
+++ carshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rhp86j7hrc6dk(-w&_#dczyq68b5jy&zaxz@$z(ytv1^q&nib*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'car100.herokuapp.com']
+ALLOWED_HOSTS = ['carshop.herokuapp.com']
 LOGIN_REDIRECT_URL='dashboard'
 
 # Application definition
