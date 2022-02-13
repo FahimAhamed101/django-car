c7cbcbc0ba55179db558c29508ab5a9f361cccd2
Tom <tomalahamed0@gmail.com>
2022-02-13 23:42:19 +0600

    jghgjg

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
 
-web: gunicorn --workers=3 carshop.wsgi --log-file -
+web: gunicorn carshop.wsgi --log-file -
--- carshop/settings.py
+++ carshop/settings.py
@@ -159,8 +159,8 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tomalahamed0@gmail.com'
-EMAIL_HOST_PASSWORD = '01706617724'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
