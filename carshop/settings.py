e7d2a483e2f6b5973269a913bf43126184d64f9c
fahim <fahimahamedweb@gmail.com>
2023-09-22 10:10:56 +0600

    djka

--- carshop/settings.py
+++ carshop/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_DIRS = [
     BASE_DIR / "media",
