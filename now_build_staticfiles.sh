95a3a9e3af7fd527408cd80fa5d9cef40ad13782
fahim <fahimahamedweb@gmail.com>
2023-09-22 23:55:15 +0600

    Added some components

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 # make migrations
 python3.9 manage.py migrate 
 python3.9 manage.py migrate sessions
-python3.9 manage.py collectstatic -l --noinput
+python3.9 manage.py collectstatic --no-input --clear
