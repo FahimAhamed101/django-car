90af4b98c1243f6c1dcbc1f3c3f1ff9cf3901afd
fahim <fahimahamedweb@gmail.com>
2023-09-22 08:43:28 +0600

    deploy

--- cars/models.py
+++ cars/models.py
@@ -57,8 +57,7 @@ class Cars(models.Model):
         ('WY', 'Wyoming'),
     )
 
-    year_choice = [(r,r) for r in range(2000, datetime.date.today().year+1)]
-
+    year_choice = [(r,r) for r in range(1984, datetime.now().year+1)]
     features_choices = (
         ('Cruise Control', 'Cruise Control'),
         ('Audio Interface', 'Audio Interface'),
