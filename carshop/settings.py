44d8863eefa98bacd09c0ceea7db57734dba2773
fahim <fahimahamedweb@gmail.com>
2023-09-22 23:51:06 +0600

    Added some components

--- carshop/settings.py
+++ carshop/settings.py
@@ -102,7 +102,12 @@ WSGI_APPLICATION = 'carshop.wsgi.application'
         'PORT': '4321',
     }
 }"""
-
+STATIC_URL = 'static/'
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",]
+    
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600),
 }
@@ -142,12 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATICFILES_DIRS = [
-#    BASE_DIR / "static",]
-    
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
