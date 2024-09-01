4cafcfa6cb2bd6ec29d4955676d896ed84e50357
fahim <fahimahamedweb@gmail.com>
2024-09-02 00:14:37 +0600

    done

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -1,7 +1,7 @@
 # now_build_staticfiles.sh
 
 # Install Python 3.6 since it is missing in the Now build environment
-pip  install -r requirements.txt
+python3  -m pip  install -r requirements.txt
 
 # make migrations
 python3 manage.py migrate 
