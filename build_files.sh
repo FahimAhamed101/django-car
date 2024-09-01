bd90ea16f80a2ffcd34bead9607f5daa2faa330b
fahim <fahimahamedweb@gmail.com>
2024-09-02 00:30:17 +0600

    done

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,19 @@ pip install -r requirements.txt
 python3.10 manage.py collectstatic -l --noinput
 vercel build --yes
 vercel --yes
+2.9.9
+"""DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'carshop',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+    }
+}"""
