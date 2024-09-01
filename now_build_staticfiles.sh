7e56921f1bee52c08e5196d8dd0cab1e2d944b06
fahim <fahimahamedweb@gmail.com>
2024-09-02 00:12:48 +0600

    done

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -1,10 +1,10 @@
 # now_build_staticfiles.sh
 
 # Install Python 3.6 since it is missing in the Now build environment
-python3 -m pip  install -r requirements.txt
+pip  install -r requirements.txt
 
 # make migrations
-python3.10 manage.py migrate 
-python3.10 manage.py migrate sessions
-python3.10 manage.py collectstatic --no-input --clear
+python3 manage.py migrate 
+python3 manage.py migrate sessions
+python3 manage.py collectstatic --no-input --clear
 DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3.9 manage.py createsuperuser --noinput
