f90553860241dd9bb911a874ba16738ed4ae73b9
fahim <fahimahamedweb@gmail.com>
2024-09-01 23:58:54 +0600

    done

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -4,7 +4,7 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py migrate sessions
-python3.9 manage.py collectstatic --no-input --clear
+python3.10 manage.py migrate 
+python3.10 manage.py migrate sessions
+python3.10 manage.py collectstatic --no-input --clear
 DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3.9 manage.py createsuperuser --noinput
