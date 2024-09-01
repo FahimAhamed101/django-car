aad3b4b2bb36aa3d35ac1f626df59f4f7e06f577
fahim <fahimahamedweb@gmail.com>
2024-09-02 03:49:27 +0600

    added

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -1,9 +1,7 @@
 # now_build_staticfiles.sh
 
 # Install Python 3.6 since it is missing in the Now build environment --no-input --clear
-python3 -m venv tutorial-env
 
-source tutorial-env/bin/activate
 pip  install -r requirements.txt
 
 
